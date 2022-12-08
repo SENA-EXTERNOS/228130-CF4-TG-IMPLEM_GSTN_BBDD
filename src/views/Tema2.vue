1faaec505ecd48be698e3dfb06a067cb5fc24cee
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2022-12-08 00:08:01 -0500

    Componente
    Componente  OK

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  .container.tarjeta.tarjeta--blanca(style="overflow:hidden").p-4.p-md-5.mb-5
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
@@ -60,8 +60,290 @@
     .row.justify-content-around.align-items-center.mb-5
       .col-2 
         img(src='@/assets/curso/temas/T2/img-7.png')
+        
       .col-9
-        p Durante los últimos años los sistemas de información c
+        .col-md-10.mx-absolute.mb-5
+          .p-4.faa
+            .bloque-texto-a__texto.p-4
+              ol.lista-ol--cuadro
+                li 
+                  .lista-ol--cuadro__vineta.fab
+                    span 1
+                  | Ingreso y lectura de información almacenados, para luego transferirlos a la memoria de la computadora.
+
+                li 
+                  .lista-ol--cuadro__vineta.fab
+                    span 2
+                  | Ingreso y escritura o grabación de datos, para que más tarde se puedan utilizar y realizar un proceso determinado. 
+
+
+    .row.justify-content-around.align-items-center.mb-5
+      
+      .col-8
+        p.mb-3 Debido a los avances que han surgido a través de la historia, los medios de almacenamiento han evolucionado en forma visible, acompañados de la evolución de las computadoras. Al inicio se contaba con muy pocos elementos de almacenamiento, prácticamente solo eran los que venían predeterminados en las computadoras. Hoy en día, además de estos, existe una diversa gama de almacenamientos externos.
+        p.mb-4 A continuación, se muestra la clasificación de los soportes de almacenamiento en cuanto a su evolución histórica. En este listado, el primero de ellos se cuenta como el más antiguo y el último como el más reciente:
+
+      .col-4
+        img(src='@/assets/curso/temas/T2/img-8.png')
+
+
+
+    .titulo-sexto.color-acento-contenido
+      p.mb-0 
+        p <b> Tabla 1. </b> <i>Elementos de almacenamientos externos</i>
+
+    .row.justify-content-around.align-items-center.mb-5
+      .tabla-a.color-acento-contenido.mb-5 
+        table
+          thead
+            tr
+              td 1. Tarjetas perforadas
+              td 10.	CD-R
+              td 19.	<i>Memory Stick</i> (MS)
+              
+          tbody
+            tr
+              td.fac 2.	Cintas perforadas
+              td.fac 11.	CD-RW
+              td.fac 20.	<i>CompactFlash</i>
+            tr
+              td 3.	Cintas magnéticas
+              td 12.	DVD
+              td 21.	<i>Microdrive</i> (MD)
+            tr
+              td.fac 4.	Discos magnéticos
+              td.fac 13.	DVD-R
+              td.fac 22.	<i>SmartMedia</i>
+            tr
+              td 5.	Disquetes
+              td 14.	DVD-RW
+              td 23.	<i>XD-Picture Card</i>
+            tr
+              td.fac 6.	Discos duros
+              td.fac 15.	<i>Blu Ray</i>
+              td.fac 24.	Disco de estado sólido
+            tr
+              td 7.	Discos duros externos
+              td 16.	Tarjetas de memoria
+              td 25.	Espacio en la nube
+            tr
+              td.fac 8.	Discos ópticos
+              td.fac 17.	Seguridad Digital (SD)
+              td.fac 
+            tr
+              td 9.	CD-ROM
+              td 18.	<i>MutiMediaCard</i> (MMC)
+              td 
+            
+
+
+
+      Separador.p3.mb-5
+
+      #t_2_1.titulo-segundo.color-acento-contenido.mb-5
+        h2 2.1 Ventajas
+
+      p.mb-5 Ventajas de salvaguardar datos en soportes:
+
+      .tarjeta
+        .row.justify-content-around.align-items-center.mb-5
+          .col-12.mt-3
+            .row.p-4.bg-celeste
+              .col-12.col-lg-8.offset-lg-1.mt-3
+                p.mb-4 La copia de seguridad se registra en un menor tiempo, porque los registros que se han hecho desde la última copia de seguridad son menos. Es decir se toma el tiempo de registrar solo los datos que han cambiado.
+              .col-3.d-none.d-lg-flex.position-relative.mt-5
+                figure.position-absolute(style="top:-75%;")
+                  img(src='@/assets/curso/temas/T2/img-9.png', alt='Texto que describa la imagen')
+            .row.justify-content-around.align-items-center.p-4.bg-azul
+              .col-3.d-none.d-lg-flex.position-absolute.mt-5
+              .col-12.col-lg-8.offset-lg-1.mt-3
+                p.mb-4 El espacio de almacenamiento que se debe usar es pequeño.
+
+            .row.justify-content-around.align-items-center.p-4.bg-celeste
+              .col-3.d-none.d-lg-flex.position-absolute.mt-5
+              .col-12.col-lg-8.offset-lg-1.mt-3
+                p.mb-4 Se pueden guardar varias versiones de un archivo.
+
+          
+      Separador.p3.mb-5
+
+      #t_2_2.titulo-segundo.color-acento-contenido.mb-5
+        h2 2.2. Desventajas
+
+      p.mb-5 Desventajas de salvaguardar datos en soportes:
+
+
+      .row.justify-content-around.align-items-center.mb-5
+        .col-sm-6.col-xl-3.mb-4.mb-xl-0
+          .tarjeta.tarjeta-slide.arriba.color-primario(@mouseover="indicadorTarjetaSlide = false")
+            .indicador--hover(v-if="indicadorTarjetaSlide")
+            .tarjeta-slide__contenedor
+              .tarjeta-slide__contenido.p-4.p-xl-5
+                p La recuperación es más lenta. Ya que para restaurar la copia de un determinado día, se tiene que restaurar la última copia completa, adicional a todos los incrementos posteriores, hasta llegar a la copia del día que se desea.
+              .tarjeta-slide__img(:style="{'background-image': `url(${require('@/assets/curso/temas/T2/img-10.png')})`}")
+
+        .col-sm-6.col-xl-3.mb-4.mb-xl-0
+          .tarjeta.tarjeta-slide.arriba.color-secundario(@mouseover="indicadorTarjetaSlide = false")
+            .indicador--hover(v-if="indicadorTarjetaSlide")
+            .tarjeta-slide__contenedor
+              .tarjeta-slide__contenido.p-4.p-xl-5
+                p Para la recuperación se hace necesario la copia completa y las copias incrementales posteriores. 
+              .tarjeta-slide__img(:style="{'background-image': `url(${require('@/assets/curso/temas/T2/img-11.png')})`}")
+
+        .col-sm-6.col-xl-3.mb-4.mb-xl-0
+          .tarjeta.tarjeta-slide.arriba.color-primario(@mouseover="indicadorTarjetaSlide = false")
+            .indicador--hover(v-if="indicadorTarjetaSlide")
+            .tarjeta-slide__contenedor
+              .tarjeta-slide__contenido.p-4.p-xl-5
+                p Para la restauración de un archivo en particular, porque necesita buscar todas las copias incrementales para revisar cuál archivo fue el que cambio.
+              .tarjeta-slide__img(:style="{'background-image': `url(${require('@/assets/curso/temas/T2/img-10.png')})`}")
+
+        .col-sm-6.col-xl-3.mb-4.mb-xl-0
+          .tarjeta.tarjeta-slide.arriba.color-secundario(@mouseover="indicadorTarjetaSlide = false")
+            .indicador--hover(v-if="indicadorTarjetaSlide")
+            .tarjeta-slide__contenedor
+              .tarjeta-slide__contenido.p-4.p-xl-5
+                p Si falla alguna de las copias de seguridad, la recuperación no es posible.
+              .tarjeta-slide__img(:style="{'background-image': `url(${require('@/assets/curso/temas/T2/img-11.png')})`}")
+
+          
+
+      Separador.p3.mb-5
+
+      #t_2_3.titulo-segundo.color-acento-contenido.mb-5
+        h2 2.3. Tipos de Recuperación de datos
+
+      p.mb-5 La recuperación de datos es un elemento fundamental en las bases de datos, ya que se encarga de registrar el recuperado del archivo. Por tal razón, la recuperación es vital a la hora en que se presentan fallos en los gestores de bases de datos; si los datos son muy importantes para la operación del negocio se hace de forma instantánea. La realización de la restauración de bases de datos resulta útil para copiar una base de datos de un servidor a otro, disponiendo protección para los datos almacenados en bases de datos. 
+
+
+
+    .bloque-texto-a.invert.p-4.p-md-5.mb-5.fnd-izq-sec
+      .row.m-0.align-items-center.justify-content-between
+        .col-lg-8
+          .bloque-texto-a__texto.p-4
+            p.mb-3 Si el administrador cuenta con una planificación adecuada, se recuperan muchos errores de medios, de usuarios y de pérdida permanente en los gestores de bases de datos. Para desarrollar un plan de copia de seguridad y restauración correcto, se debe analizar qué datos son sensibles para la operación del negocio de la empresa, qué posible impacto de la pérdida, generan esos datos. También es conveniente crear un plan de recuperación de desastres para garantizar que todos los sistemas y datos puedan recuperarse rápidamente. Es obligación del administrador de datos tener esa planificación, para realizar su operación en caso de algún fallo en los SGBD. 
+        .col-lg-4.mb-4.mb-lg-0
+          figure
+            img(src='@/assets/curso/temas/T2/img-12.png', alt='')
+
+
+    .row.justify-content-around.align-items-center.mb-5
+      .col-1
+        img(src='@/assets/curso/temas/T2/img-13.svg')
+
+      .col-md.col-lg-11
+        p.mb-4 Para el servidor SQL existen tres formas de recuperación de registros para cada base de datos. Cada una de ellas determina la lógica de cómo se realizan las copias de seguridad de los datos y cuál es el riesgo de perder datos. El primer modelo de recuperación simple, permite recuperar la base de datos hasta la última copia de seguridad que se había registrado en ella. Por otra parte, el modelo de recuperación completa realiza la recuperación de la base de datos, hasta el momento del error y las operaciones de log hasta el momento de la caída y, en la forma de recuperación de registro masivo, recupera hasta el último registro del log. A continuación, se explican cada una de ellas:
+
+
+
+
+
+
+    .row.justify-content-center.mb-3
+      .col-5
+        figure
+          img(src="@/assets/curso/temas/T2/img-14.png", alt="Texto que describa la imagen")
+      .col-7
+        AcordionA.mb-5(tipo="a" clase-tarjeta="tarjeta tarjeta--azul")
+          .row(titulo="Recuperación simple")
+            p para esta recuperación, solo se genera la copia de seguridad de los datos que han cambiado desde el último <i>backup</i> que se haya registrado en la base de datos. Con este método se pueden realizar copias de seguridad más constantes, porque estas copias de seguridad diferenciales son más pequeñas y rápidas, que las copias de seguridad de toda una base de datos. Es importante recordar que realizando copias de seguridad con frecuencia se disminuye el riesgo de perder datos.
+
+          .row(titulo="Recuperación completa")
+            p en las copias de seguridad completa de base de datos, se utiliza la sentencia <i>RESTORE DATABASE</i> usando como configuración o sentencia, el nombre de la base de datos que se va a restaurar y el dispositivo de copia de seguridad donde se restaurará la copia de seguridad de la base de datos. Adicionalmente, se utiliza la sentencia <i>NORECOVERY</i>, esto con el fin de aplicar una copia de seguridad de registro de transacciones, o una copia de seguridad diferencial de base de datos después de restaurar la copia de seguridad de la base de datos.
+
+          .row(titulo="Recuperación de registro masivo")
+            p  permite realizar operaciones de recuperación con lotes de alto rendimiento. Son necesarias las copias de seguridad de registros de transacciones, pero la recuperación sólo se extiende hasta el momento de efectuar la copia de seguridad.
+
+
+    Separador.p3.mb-5
+
+    #t_2_4.titulo-segundo.color-acento-contenido.mb-5
+      h2 2.4. Mecanismos de verificación de la integridad de copias de seguridad
+
+
+
+
+
+    .row.justify-content-around.align-items-center.mb-5
+      
+      .col-md.col-lg-8
+        p Las bases de datos realizan altos procesamientos de registros, para lo cual son necesarias las copias de seguridad o <i>backup</i>. Estas se realizan con el objetivo de tener copias fieles de los datos originales de un sistema en un momento determinado y tener la posibilidad de gestionar su recuperación, en caso de producirse fallos que generen pérdidas y a su vez, generen inconsistencia de datos. Dichas pérdidas se pueden dar por desastres naturales, que no se pueden evitar, errores humanos o deterioros de <i>software</i> o <i>hardware</i>, que enmarcan agentes tanto externos como internos, como podrían ser los virus informáticos o archivos corrompidos, o las no maliciosas, en las que normalmente son usuarios que no están capacitados, para el manejo de la base de datos o usuarios bien intencionados que eliminan datos.
+        .cajon.color-acento-contenido.p-4.mb-4.color-botones-amarillo
+          p Por lo anterior, es necesario tener mecanismos de copia de seguridad que garanticen que los datos estén siempre disponibles, como debe ser. Las copias de seguridad no tendrían sentido si no existe un sistema de restauración de datos, si no existieran estas opciones en los SGDB, simplemente los sistemas de información serían muy vulnerables, porque al dañarse una base de datos dejaría de existir. En consecuencia, muchas empresas tendrían que parar sus operaciones. 
+
+
+      .col-4.col-sm-6.col-md-4.mb-4.mb-md-0
+        img(src='@/assets/curso/temas/T2/img-15.png')
+
+
+
+    p.mb-5 El mecanismo diseñado para la copia de seguridad es fundamental en la protección de datos o registros para la continuidad y recuperación de los datos de una empresa sin alteraciones. Los mecanismos de copia de seguridad siguen estos factores que se muestran a continuación:
+
+
+
+    .row.justify-content-around.align-items-center.mb-5
+      .col-sm-6.col-lg-3.mb-4.mb-lg-0
+        .tarjeta.tarjetas-botones-f.color-primario.p-4
+          .row.justify-content-around.align-items-center.mb-3
+            .col-7
+              figure
+                img(src='@/assets/curso/temas/T2/img-16.svg', alt='Texto que describa la imagen')
+
+          p.text-small Cantidad de tiempo para realizar las copias de seguridad.
+
+      .col-sm-6.col-lg-3.mb-4.mb-lg-0
+        .tarjeta.tarjetas-botones-f.color-primario.p-4
+          .row.justify-content-center.mb-3
+            .col-7
+              figure
+                img(src='@/assets/curso/temas/T2/img-17.svg', alt='Texto que describa la imagen')
+
+          p.text-small Metodología para registrar copias de seguridad.
+
+      .col-sm-6.col-lg-3.mb-4.mb-lg-0
+        .tarjeta.tarjetas-botones-f.color-primario.p-4
+          .row.justify-content-center.mb-3
+            .col-7
+              figure
+                img(src='@/assets/curso/temas/T2/img-18.svg', alt='Texto que describa la imagen')
+
+          p.text-small Según el fallo, cuál sería el plan de recuperación a seguir para restablecer el funcionamiento.
+
+
+
+    .bloque-texto-a.fae.p-4.p-md-5.mb-5.img-bg
+      .row.m-0.justify-content-between
+        .col-lg-4.mb-4.mb-lg-0
+          
+        .col-lg-8
+          .bloque-texto-a__texto.p-4
+            p.mb-3 Es importante anotar que las copias de seguridad son parte de las tareas de un administrador de base de datos, siendo esta una labor constante. Este hecho se da porque los datos de las organizaciones están expuestos a fallos o daños irreparables, que deben ser recuperados de manera inmediata, una empresa no permite datos eliminados por fallos porque esto se traduciría en pérdidas económicas y desconfianza de sus clientes, por esta razón las copias de seguridad son tareas permanentes.
+            p A continuación, se identifican los tipos de copias de seguridad:
+
+
+
+
+
+    .row.justify-content-center.mb-3
+      .col-7
+        AcordionA.mb-5(tipo="a" clase-tarjeta="tarjeta tarjeta--azul")
+          .row(titulo="Copia de seguridad completa")
+            p el objetivo es realizar una copia en otro medio de almacenamiento. Al copiar todos los datos, la copia de seguridad completa puede llegar a presentar lentitud en la operación de la base de datos. Además de necesitar grandes dispositivos de almacenamiento. Este tipo de copia es recomendable realizarla cada mes.
+
+          .row(titulo="Copia de seguridad incremental")
+            p son las copias que se realizan con más frecuencia, ya que se hace la copia desde el punto donde se registró la última copia de seguridad, haciéndola más rápida que la anterior, puesto que se hace copia de los nuevos datos registrados. Si en algún momento se hiciera necesario una copia completa, se necesitan todas las copias anteriores para dejar los datos originalmente. Este hecho se daría en caso de pérdida absoluta de datos dentro de una base de datos.
+
+          .row(titulo="Copia de seguridad diferencial")
+            p la copia de seguridad diferencial se enfoca únicamente en los registros nuevos o los que fueron modificados. Es una copia más básica, se registran en algunas situaciones desde la última copia en modo incremental.  
+          
+          .row(titulo="Copia de seguridad delta")
+            p.mb-3 son copias de seguridad que se van dando cuando la base de datos tiene incrementos en sus registros, estas guardan bloques de datos en archivos individuales.
+            p.mb-2 Se debe tener resuelto el problema de la planificación de cada cuánto tiempo se va a hacer una copia de seguridad. Esto va a depender de la importancia que se le dé a la información almacenada en el equipo y del mayor o menor movimiento que se realice de los datos.
+
+      .col-5
+        figure
+          img(src="@/assets/curso/temas/T2/img-20.png", alt="Texto que describa la imagen")
+
 
 
 
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -5,7 +5,148 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 3
-      h1 Titulo de primer nivel
+      h1 Documentación de control y accesos
+
+
+    .row.justify-content-around.align-items-center.mt-5
+      .col-10.px-5.py-4
+        .row
+          .col-12.d-lg-block
+            .bloque-texto-x.fad.p-3.p-sm-4.p-md-5.mb-5
+              .bloque-texto-x__img(data-aos='fade-right')(
+                :style="{'background-image': `url(${require('@/assets/curso/temas/T3/img-1.png')})`}"
+              )
+              .bloque-texto-a__texto.p-4
+                p.mb-3 diferentes. Si bien la seguridad hace referencia a la protección de los datos, es decir, que los datos que estén dentro de una base de datos no tengan cambios si no están permitidos; visto desde el punto de vista de su alteración o destrucción, no autorizadas por parte del administrador. Por otra parte, la integridad hace referencia a la precisión o validez de esos datos.  
+                p.mb-5 A continuación, se muestran los significados de seguridad e integridad:
+
+
+    .tarjeta--container.row.mb-5
+      .col-md.tarjeta.color-primario.p-5
+        .row.justify-content-around.align-items-center.mb-4
+          .col-6
+            figure
+              img(src='@/assets/curso/temas/T3/img-2.svg', alt='Texto que describa la imagen')
+        h4.text-center Seguridad
+        p  protección de datos ante usuarios no autorizados.
+      .col-md.tarjeta.color-secundario.p-5
+        .row.justify-content-around.align-items-center.mb-4
+          .col-6
+            figure
+              img(src='@/assets/curso/temas/T3/img-3.svg', alt='Texto que describa la imagen')
+        h4.text-center Integridad
+        p protección de usuarios autorizados. 
+
+
+
+    .row.justify-content-around.align-items-center.mb-5
+      
+      .col-md.col-lg-8
+        p Según el concepto anterior, la seguridad es la encargada de garantizar que los usuarios tengan el permiso de realizar tareas específicas; mientras que la integridad, involucra asegurar que las tareas que están gestionando, se realicen de forma correcta.
+        .cajon.color-acento-contenido.p-4.mb-4.color-botones-amarillo
+          p Como son conceptos que interfieren en un mismo objetivo existen similitudes: en los dos casos el sistema necesita registrar restricciones para los usuarios. Además para ambos casos, estas restricciones deben ser especificadas por el administrador de base de datos en un lenguaje adecuado y deben estar dentro del catálogo del sistema. Adicionalmente, en ambos casos el SGBD vigila las operaciones del usuario, para asegurarse que cumpla las restricciones impuestas. 
+ 
+
+
+      .col-4.col-sm-6.col-md-4.mb-4.mb-md-0
+        img(src='@/assets/curso/temas/T3/img-4.png')
+
+
+
+
+    .bloque-texto-a.faf.p-4.p-md-5.mb-5.img-bg-seg
+      .row.m-0.justify-content-between
+        .col-lg-4.mb-4.mb-lg-0
+          
+        .col-lg-8
+          .bloque-texto-a__texto.p-4
+            p.mb-3 La seguridad de los registros en un Sistema Gestor de Base de Datos (SGBD) es un aspecto importante para las organizaciones, su objetivo es proteger los datos de personas que no están autorizadas para el uso de ellos. 
+            p Claramente, la información es un elemento que debe tener especial cuidado, porque representa un valor importante para las empresas, perderla significa una disminución en las operaciones del negocio. Se debe recordar que la información les da la posibilidad a las empresas de gestionar negocios entre sus clientes. 
+
+
+    p.mb-5 A continuación, se ven algunos aspectos importantes que, si no se tienen en cuenta, pueden vulnerar la seguridad de los datos en una empresa:
+
+
+
+    .titulo-sexto.color-acento-contenido
+      p.mb-0 
+        p <b> Figura 2. </b> <i>Seguridad de los registros en un Sistema Gestor de Base de Datos (SGBD)</i>
+
+
+    ImagenInfografica.color-secundario.mb-5
+      template(v-slot:imagen)
+        figure
+          img(src='@/assets/curso/temas/T3/img-6.png', alt='Texto que describa la imagen')
+
+      .tarjeta.color-botones-calientes-seg.p-3(x="5.6%" y="31%" numero="1")
+        .h5.mb-2 Aspectos legales, sociales y éticos
+        p por ejemplo, la persona que hace la petición ¿tiene derecho legal para conocer la información solicitada como, el crédito de un cliente?
+
+      .tarjeta.color-botones-calientes-seg.p-3(x="21.2%" y="71.5%" numero="2")
+        .h5.mb-2 Controles físicos
+        p por ejemplo, ¿el lugar en donde se encuentra la computadora o terminal está bajo llave o con alguna otra protección?
+
+      .tarjeta.color-botones-calientes-seg.p-3(x="35.6%" y="31%" numero="3")
+        .h5.mb-2 Cuestiones de política
+        p por ejemplo, ¿cómo decide la empresa propietaria del sistema a quién y a qué se le permite tener acceso?
+
+      .tarjeta.color-botones-calientes-seg.p-3(x="50.8%" y="71.5%" numero="4")
+        .h5.mb-2 Problemas operacionales
+        p por ejemplo, si se utiliza un esquema de contraseñas, ¿cómo se les mantiene en secreto? ¿con cuánta frecuencia son cambiadas?
+
+      .tarjeta.color-botones-calientes-seg.p-3(x="65.2%" y="31%" numero="5")
+        .h5.mb-2 Controles de <i>hardware</i>
+        p por ejemplo, ¿la unidad de procesamiento proporciona alguna característica de seguridad, como claves de protección de almacenamiento o un modo de operación protegida?
+
+      .tarjeta.color-botones-calientes-seg.p-3(x="80%" y="71.5%" numero="6")
+        .h5.mb-2 Soporte del sistema operativo
+        p por ejemplo, ¿el sistema operativo subyacente borra el contenido de la memoria principal y los archivos de disco, cuando ha terminado de utilizarlos?
+
+      .tarjeta.color-botones-calientes-seg.p-3(x="94.5%" y="31%" numero="7")
+        .h5.mb-2 Los asuntos que conciernen únicamente al sistema de base de datos
+        p por ejemplo, ¿tiene el sistema de base de datos un concepto de propiedad de los datos?
+
+
+
+    .row.justify-content-around.align-items-center.mb-5
+      .col-1
+        img(src='@/assets/curso/temas/T3/img-7.svg')
+
+      .col-md.col-lg-11
+        p.mb-4 Hay dos enfoques que dirigen la seguridad de los datos, los cuales buscan proteger los datos de una base de datos de usuarios mal intencionados, o simplemente de cometer errores de borrado de registros. Estos controles se conocen como: 
+        
+
+
+
+    .tarjeta.tarjeta-slyder.p-4.mb-5
+      SlyderA(tipo="b")
+        .row.justify-content-around.align-items-center
+          .col-md-6.mb-4.mb-md-0
+            h3.mb-3 Control de acceso discrecional: 
+            p.mb-3 el control de acceso discrecional (DAC) es un mecanismo de <i>software</i> o del SGBD, para controlar el acceso de los usuarios a archivos y directorios. Además, el control de acceso discrecional deja que la configuración de protecciones para archivos y directorios, sean realizadas por el administrador de la base de datos según su criterio. Hay dos formas para ejercer el control de acceso discrecional, una son los <i>bits</i> de permisos UNIX y las listas de control de acceso (ACL).
+            p Los <i>bits</i> permiten que el administrador establezca protección de lectura, escritura y ejecución, si así lo desea por propietario, grupo y otros usuarios. Por otra parte, el sistema UNIX, permite a el superusuario o usuario <i>root</i> registrar la protección de DAC.
+
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/T3/img-8.png', alt='Texto que describa la imagen')
+
+        .row.justify-content-around.align-items-center
+          .col-md-6.mb-4.mb-md-0
+            h3.mb-3 Control de acceso obligatorio
+            p.mb-4 para las copias de seguridad completas de base de datos, se utiliza la sentencia <i>RESTORE DATABASE</i>, usando como configuración o sentencia el nombre de la base de datos que se va a restaurar y el dispositivo de copia de seguridad donde se restaurará la copia de seguridad de la base de datos. 
+            p Adicionalmente, se utiliza la sentencia <i>NORECOVERY</i>, esto con el fin de aplicar una copia de seguridad de registro de transacciones o una copia de seguridad diferencial de base de datos, después de restaurar la copia de seguridad de la base de datos. 
+        
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/T3/img-9.png', alt='Texto que describa la imagen')
+
+
+
+
+
+
+
+
 
 </template>
 
